bde86e9276754d0d36eb087a4b3cdf6ffa92d977
mialarisa9 <mia_larisa@apps.ipb.ac.id>
2018-12-05 17:01:08 +0700

    pi

---
+++ DoodleRun/Assets/Script/PlayerControllers.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllers : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpForce;
+
+	private Rigidbody2D myRigidbody;
+
+	public bool grounded;
+	public LayerMask whatIsGround;
+
+	private Collider2D myCollider;
+
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody2D>();
+
+		myCollider = GetComponent<Collider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+
+		if(grounded){
+		myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
+		}
+		
+		if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+		{
+			if(grounded)
+			{
+			myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+			}
+		}
+	}
+	
+}
